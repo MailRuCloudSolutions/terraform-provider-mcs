58a24f6e6fb2bd44acef28dabc04b2ea2ef8aab7
irlndts <kleto4kin@gmail.com>
2021-07-01 17:34:45 +0300

    cleanup

--- examples/create-mcs-cluster/vars.tf
+++ examples/create-mcs-cluster/vars.tf
@@ -1,21 +1,6 @@
-variable "k8s-template-test" {
-    type = string
-    default = "9e7a9856-4e63-4d38-b105-65850df5eb4e"
-}
-
-variable "k8s-template" {
-    type = string
-    default = "95663bae-6763-4a53-9424-831975285cc1"
-}
-
 variable "public-key-file" {
     type = string
     default = "~/.ssh/id_rsa.pub"
-}
-
-variable "k8s-flavor-test" {
-    type = string
-    default = "Basic-1-1-10"
 }
 
 variable "k8s-flavor" {
